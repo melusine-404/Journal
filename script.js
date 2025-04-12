5018f3e8c258d55d7d1a5ba330ab82e1ddbf1c2a
melusine-404 <jferrarig@icloud.com>
2025-04-13 00:24:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function checkPassword() {
     document.getElementById("password-screen").classList.add("hidden");
     document.getElementById("circle").classList.remove("hidden");
   } else {
-    alert("Mot de passe incorrect");
+    alert("Oups");
   }
 }
 
@@ -15,64 +15,38 @@ function loadEntry(name) {
     .then(data => {
       document.getElementById("entry-content").innerText = data;
     });
-}
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Mélusine - Portes</title>
+  <link rel="stylesheet" href="style.css">
+  <script defer src="script.js"></script>
+</head>
+<body>
+  <h1>Mélusine</h1>
 
-const sparkleCanvas = document.createElement('canvas');
-sparkleCanvas.id = 'sparkleCanvas';
-document.body.appendChild(sparkleCanvas);
-const ctx = sparkleCanvas.getContext('2d');
+  <div class="doors-container">
+    <div class="door" onclick="openDoor(this, 'journal.html')">
+      <div class="door-inner">
+        <div class="door-front" style="background-image: url('assets/icons/porte1.png');"></div>
+      </div>
+      <div class="door-title">Pensées</div>
+    </div>
 
-let width, height;
-function resize() {
-  width = window.innerWidth;
-  height = window.innerHeight;
-  sparkleCanvas.width = width;
-  sparkleCanvas.height = height;
-}
-window.addEventListener('resize', resize);
-resize();
+    <div class="door" onclick="openDoor(this, 'saisons.html')">
+      <div class="door-inner">
+        <div class="door-front" style="background-image: url('assets/icons/porte2.png');"></div>
+      </div>
+      <div class="door-title">Photos</div>
+    </div>
 
-const sparkles = [];
-const sparkleCount = 120;
-const mouse = { x: width / 2, y: height / 2 };
-
-window.addEventListener('mousemove', e => {
-  mouse.x = e.clientX;
-  mouse.y = e.clientY;
-});
-
-function createSparkle() {
-  return {
-    x: Math.random() * width,
-    y: Math.random() * height,
-    r: Math.random() * 1.5 + 0.5,
-    dx: (Math.random() - 0.5) * 0.5,
-    dy: (Math.random() - 0.5) * 0.5,
-    a: Math.random() * Math.PI * 2,
-    opacity: Math.random() * 0.6 + 0.4
-  };
-}
-
-for (let i = 0; i < sparkleCount; i++) {
-  sparkles.push(createSparkle());
-}
-
-function draw() {
-  ctx.clearRect(0, 0, width, height);
-  for (let s of sparkles) {
-    s.x += s.dx + (mouse.x - width / 2) * 0.0002;
-    s.y += s.dy + (mouse.y - height / 2) * 0.0002;
-
-    if (s.x < 0 || s.x > width || s.y < 0 || s.y > height) {
-      Object.assign(s, createSparkle());
-    }
-
-    ctx.beginPath();
-    ctx.arc(s.x, s.y, s.r, 0, Math.PI * 2);
-    ctx.fillStyle = `rgba(218,165,32,${s.opacity})`; // doré
-    ctx.fill();
-  }
-
-  requestAnimationFrame(draw);
-}
-draw();
+    <div class="door" onclick="openDoor(this, 'meteo.html')">
+      <div class="door-inner">
+        <div class="door-front" style="background-image: url('assets/icons/porte3.png');"></div>
+      </div>
+      <div class="door-title">Dessins</div>
+    </div>
+  </div>
+</body>
+</html>
