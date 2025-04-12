fea6ddfaeb1d3d6a829bc2d28a8fd1ad66e7cc54
melusine-404 <jferrarig@icloud.com>
2025-04-13 00:26:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,44 +9,9 @@ function checkPassword() {
   }
 }
 
-function loadEntry(name) {
-  fetch(`entries/${name}.txt`)
-    .then(res => res.text())
-    .then(data => {
-      document.getElementById("entry-content").innerText = data;
-    });
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  <meta charset="UTF-8">
-  <title>Mélusine - Portes</title>
-  <link rel="stylesheet" href="style.css">
-  <script defer src="script.js"></script>
-</head>
-<body>
-  <h1>Mélusine</h1>
-
-  <div class="doors-container">
-    <div class="door" onclick="openDoor(this, 'journal.html')">
-      <div class="door-inner">
-        <div class="door-front" style="background-image: url('assets/icons/porte1.png');"></div>
-      </div>
-      <div class="door-title">Pensées</div>
-    </div>
-
-    <div class="door" onclick="openDoor(this, 'saisons.html')">
-      <div class="door-inner">
-        <div class="door-front" style="background-image: url('assets/icons/porte2.png');"></div>
-      </div>
-      <div class="door-title">Photos</div>
-    </div>
-
-    <div class="door" onclick="openDoor(this, 'meteo.html')">
-      <div class="door-inner">
-        <div class="door-front" style="background-image: url('assets/icons/porte3.png');"></div>
-      </div>
-      <div class="door-title">Dessins</div>
-    </div>
-  </div>
-</body>
-</html>
+function openDoor(element, link) {
+  element.classList.add('open');
+  setTimeout(() => {
+    window.location.href = link;
+  }, 1000); // durée de l’animation avant redirection
+}
